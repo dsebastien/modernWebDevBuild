62ed988d1b40ee2f453e2643f2a4991447a101be
Sebastien <seb@dsebastien.net>
2015-09-19 10:58:19 +0200

    Updated package config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,7 @@
 /*
  Rather than manage one giant configuration file responsible
- for creating multiple tasks, each task has been broken out into
- its own file in gulp/tasks. Any files in that directory get
- automatically required below.
+ for creating multiple gulp tasks, each task has been broken out into
+ its own file. Any files in that directory get automatically required below.
 
  To add a new task, simply add a new task file that directory.
  gulp/tasks/default.js specifies the default set of tasks to run
